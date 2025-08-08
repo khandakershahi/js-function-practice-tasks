ed9bfdd1ed016125bc2971dc35077f2e81587a06
khandakershahi <khandakershahi@gmail.com>
2025-08-09 00:30:10 +0600

    task 1 done

---
+++ task-1.js
@@ -0,0 +1,11 @@
+/**
+ * ### Task-1  
+ *Take four parameters. Multiply the four numbers and then return the result 
+
+ */
+
+ function multiply(a, b, c, d){
+    return a * b * c * d;
+ }
+
+ console.log(multiply(1,2,3,4));
